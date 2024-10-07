28674c60c8efbf6f72043e039246ec3d722d6aeb
Aadil Ayub <aadilayub@protonmail.com>
2024-10-07 21:52:49 +0500

    Implement input box

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function TextInput() {
+  const [guess, setGuess] = React.useState('');
+  return (
+    <form 
+    onSubmit={e => {
+      e.preventDefault();
+      console.log(guess);
+      setGuess('');
+    }}
+    className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input 
+        value={guess}
+        onChange={e => setGuess(e.target.value.toUpperCase())} 
+        id="guess-input" 
+        type="text"
+        pattern="[A-Z]{5}"
+      />
+    </form>
+  );
+}
+
+export default TextInput;
